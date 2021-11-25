13a61820254f484ebf676470b2a759d759e02bf2
TTmesb <emil.aberg@elev.ga.ntig.se>
2021-11-25 13:37:51 +0100

    yaa

---
+++ src/Controller.java
@@ -0,0 +1,7 @@
+/**
+ * Skapat november 2021-11-15 - 09:36
+ * Controller
+ * Skriven av coolingen
+ */
+public class Controller {
+}
---
+++ src/Model.java
@@ -0,0 +1,7 @@
+/**
+ * Skapat november 2021-11-15 - 08:43
+ * Model
+ * Skriven av coolingen
+ */
+public class Model {
+}
